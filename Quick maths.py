0b7074d4ef0198ac1813ae07a5beeeb7ee341500
NMRobinson92 <35815367+NMRobinson92@users.noreply.github.com>
2018-02-21 20:06:22 +0000

    Add files via upload

---
+++ Quick maths.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 20 19:57:49 2018
+
+@author: nathanrobinson
+"""
+
+print('Hello World')
+print ('i LiKe 6.00.1x?')
+5
+type(5)
+3.5
+type(3.5)
+print("3+5")
+print('')
+x=3
+if x*2 == 5:
+    print('')
+    print('why')
+    
+else:
+    print('yo')
+print ('')
+print('im finished')
+
+if x*6 == 18:
+    print('')
+    print('inbetween',x*5)
+    if x*3 != 9:
+        print ('yoooooooo')
+    else:
+        print ('nooope')
+
+y = 4
+z = 5
+if x < y and x > z:
+    print ('yeah correct')
+elif y < x:
+    print ('something extra')
+else:
+    print ('yo ma ma', z+y-x)
+
+print ('')
+
+a = 3
+a = a*a
+b = a+x
+print(a) 
+print (b)
+
+
+
